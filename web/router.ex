c15551c1117ca07061e49b3c796cd05cd4520649
Carlos Saito <c.saito.style@gmail.com>
2017-01-10 18:04:50 +0100

    Add route /players

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,8 @@ defmodule Ipo.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
+    resources "/players", PlayerController
   end
 
   # Other scopes may use custom stacks.
