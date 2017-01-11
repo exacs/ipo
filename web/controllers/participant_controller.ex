9c049458e079d6b4c3457f422d06276c8b784274
Carlos Saito <c.saito.style@gmail.com>
2017-01-11 22:55:35 +0100

    Sketch of Pairings

--- web/controllers/participant_controller.ex
+++ web/controllers/participant_controller.ex
@@ -32,7 +32,26 @@ defmodule Ipo.ParticipantController do
     end
   end
 
-  def matches_index(conn, _params) do
+  def matches_index(conn, %{"tournament_id" => tournament_id}) do
+    tournament = Repo.get!(Tournament, tournament_id)
+    |> Repo.preload([:participants])
+
+    participants = tournament.participants
+    |> Repo.preload([:team])
+
+    changeset = Tournament.changeset(%Tournament{})
+
+    positions = get_positions(participants |> length)
+
+    render(
+      conn,
+      "matches_index.html",
+      tournament: tournament,
+      participants: participants,
+      positions: positions,
+      cross: Enum.zip(positions, participants),
+      changeset: changeset
+    )
   end
 
   def delete(conn, %{"id" => id, "tournament_id" => tournament_id}) do
@@ -46,5 +65,23 @@ defmodule Ipo.ParticipantController do
     conn
     |> put_flash(:info, "Team deleted successfully.")
     |> redirect(to: tournament_path(conn, :show, tournament))
+  end
+
+  #####
+  def get_positions(n) do
+    pow2 = &(:math.pow(2, &1))
+    m = :math.log2(n)
+    |> Float.ceil
+    |> pow2.()
+    |> trunc
+
+    case m do
+      1 -> [1,2]
+      2 -> [1,2]
+      _ ->
+        get_positions(div(m, 2))
+        |> Enum.map(fn(i) -> [i, m+1-i] end)
+        |> List.flatten
+    end
   end
 end
